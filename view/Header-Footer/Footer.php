d3780de11bc176f264ba103feca6a663cc36ceb3
Daniel250Gue <84732717+Daniel250Gue@users.noreply.github.com>
2022-10-22 17:37:57 -0500

    Parte 1

---
+++ view/Header-Footer/Footer.php
@@ -0,0 +1,54 @@
+<footer id="footer">
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-6 col-md-6 footer-info">
+            <img src="public/img/logo-utp-white.png" alt="UTP">
+            <p>Universidad Tecnológica de Panamá - 2022 <br>
+              Dirección: Avenida Universidad Tecnológica de Panamá, Vía Puente Centenario,
+              Campus Metropolitano Víctor Levi Sasso. <br>
+              Teléfono. (507) 560-3000 <br>
+              Correo electrónico: buzondesugerencias@utp.ac.pa <br>
+              311 Centro de Atención Ciudadana <br>
+              Políticas de Privacidad</p>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <ul>
+              <li><i class="fa fa-angle-right"></i> <a href="#">Inicio</a></li>
+              <li><i class="fa fa-angle-right"></i> <a href="#">Sobre el Congreso</a></li>
+              <li><i class="fa fa-angle-right"></i> <a href="#">Lugar de Encuentro</a></li>
+              <li><i class="fa fa-angle-right"></i> <a href="#">Áreas de Interés</a></li>
+              <li><i class="fa fa-angle-right"></i> <a href="#">Contáctanos</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <ul>
+              <li><i class="fa fa-angle-right"></i> <a href="#">ECA</a></li>
+              <li><i class="fa fa-angle-right"></i> <a href="#">JIC</a></li>
+              <li><i class="fa fa-angle-right"></i> <a href="#">WPA</a></li>
+              <li><i class="fa fa-angle-right"></i> <a href="#">UTP</a></li>
+              <li><i class="fa fa-angle-right"></i> <a href="#">Mapa de Ubicación</a></li>
+            </ul>
+
+            <div class="social-links">
+              <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
+              <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
+              <a href="#" class="instagram"><i class="fa fa-instagram"></i></a>
+              <a href="#" class="google-plus"><i class="fa fa-linkedin"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="copyright">
+        &copy; 2024 Copyright <strong>Universidad Tecnológica de Panamá | </strong> All Rights Reserved by UTP.
+      </div>
+      <div class="credits">
+      </div>
+    </div>
+  </footer>
--- view/itinerario.php
+++ view/itinerario.php
@@ -1,72 +1,335 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Itinerario</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <link rel="stylesheet" href="itinerario.css">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Congreso UTP - 2024</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <link href="public/img/logo-utp-white.png" rel="icon">
+  <link href="public/img/logo-utp-white.png" rel="icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Raleway:300,400,500,700,800" rel="stylesheet">
+
+  <!-- Bootstrap -->
+  <link href="public/css/bootstrap/bootstrap.css" rel="stylesheet">
+  <link href="public/css/bootstrap/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Librarias CSS -->
+  <script src="https://kit.fontawesome.com/3f6fcc0d63.js" crossorigin="anonymous"></script>
+  <link href="public/css/font-awesome/font-awesome.css" rel="stylesheet">
+  <link href="public/css/font-awesome/font-awesome.min.css" rel="stylesheet">
+  <link href="public/css/animate/animate.min.css" rel="stylesheet">
+  <link href="public/css/venobox/venobox.css" rel="stylesheet">
+  <link href="public/css/owlcarousel/owl.carousel.min.css" rel="stylesheet">
+
+  <!-- CSS -->
+  <link href="public/css/style.css" rel="stylesheet">
 </head>
+
 <body>
-    <section class="bloque">
-        <h1>Itinerario del Congreso</h1>
-    </section>
-    <center>
-    <section class="todo">
-        
-        <div class="container col-7">
-            <p class="col-sm-3">Tema de Charla Aquí</p>
-            <div class="col-12 col-sm-6">8:00 am - 9:00 am | Tema 1</div>
-            <div class="w-100"></div>
+  <!-- Header -->
+  <header id="header">
+    <div class="container">
+      <div id="logo" class="pull-left">
+        <a href="index.html" class="scrollto"><img src="public/img/logo-utp-white.png" alt="UTP" title=""></a>
+      </div>
+      <?php
+      require_once 'view/Header-Footer/Header.php';
+      ?>
+    </div>
+  </header>
 
-            <div class="col-12 col-sm-6">9:00 am - 10:00 am | Tema 2</div>
-            <div class="w-100"></div>
-
-            <div class="col-12 col-sm-6">10:00 am - 11:00 am | Tema 3</div>
-            <div class="w-100"></div>
-
-            <div class="col-12 col-sm-6">11:00 am - 12:00 pm | Tema 4</div>
-            <div class="w-100"></div>
-
-            <div class="col-12 col-sm-6">12:00 pm - 1:00 pm | Tema 5</div>
-            <div class="w-100"></div>
-
-            <div class="col-12 col-sm-6">1:00 pm - 2:00 pm | Tema 6</div>
-            <div class="w-100"></div>
-
-            <div class="col-12 col-sm-6">2:00 pm - 3:00 pm | Tema 7</div>
-            <div class="w-100"></div>
-
-            <div class="col-12 col-sm-6">3:00 pm - 4:00 pm | Tema 8</div>
-            <div class="w-100"></div>
-
-            <div class="col-12 col-sm-6">4:00 pm - 5:00 pm | Tema 9</div>
-            <div class="w-100"></div>
-
-            <div class="col-12 col-sm-6">5:00 pm - 6:00 pm | Tema 10</div>
-            <div class="w-100"></div>
-
-            <div class="col-12 col-sm-6">7:00 pm - 8:00 pm | Tema 11</div>
-            <div class="w-100"></div>
+  <!-- Contenido -->
+    <!-- Cronograma -->
+    <section id="schedule" class="section-with-bg">
+      <div class="container wow fadeInUp">
+        <div class="section-header">
+          <h2>Cronograma</h2>
+          <p>Este será nuestro cronograma de conferencias</p>
         </div>
+        <ul class="nav nav-tabs" role="tablist">
+          <li class="nav-item">
+            <a class="nav-link active" href="#day-1" role="tab" data-toggle="tab">Día 1</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#day-2" role="tab" data-toggle="tab">Día 2</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#day-3" role="tab" data-toggle="tab">Día 3</a>
+          </li>
+        </ul>
 
-
-
-        <div class="container col-5">
-            <form>
-                <input type="date" name="" id="">
-            </form>
-        </div>
-
-
-    </section>
-    </center>
-    <section class="bloque final">
         
+        
+          <div class="tab-content row justify-content-center">
+          <!-- Dia 1 -->
+          <div role="tabpanel" class="col-lg-9 tab-pane fade show active" id="day-1">
+
+            <div class="row schedule-item">
+              <div class="col-md-2"><time>09:30 AM</time></div>
+              <div class="col-md-10">
+                <h4>Bienvenida. </h4>
+                <p>Bienvenida de los Organizadores del Congreso.</p>
+              </div>
+            </div>
+
+            <div class="row schedule-item">
+              <div class="col-md-2"><time>10:00 AM</time></div>
+              <div class="col-md-10">
+                <div class="speaker">
+                  <img src="public/img/speakers/1.jpg" alt="Brenden Legros">
+                </div>
+                <h4>La Innovación. <span>Brenden Legros</span></h4>
+                <p>El Imperativo de Innovar para un desarrollo inclusivo y sustentable.</p>
+              </div>
+            </div>
+
+            <div class="row schedule-item">
+              <div class="col-md-2"><time>11:00 AM</time></div>
+              <div class="col-md-10">
+                <div class="speaker">
+                  <img src="public/img/speakers/2.jpg" alt="Hubert Hirthe">
+                </div>
+                <h4>Topografía de superficie. <span>Hubert Hirthe</span></h4>
+                <p>Uso de la Biología Básica para informar el Diseño de Dispositivos Médicos: Topografía de superficie.</p>
+              </div>
+            </div>
+
+            <div class="row schedule-item">
+              <div class="col-md-2"><time>12:00 AM</time></div>
+              <div class="col-md-10">
+                <div class="speaker">
+                  <img src="public/img/speakers/3.jpg" alt="Cole Emmerich">
+                </div>
+                <h4>Esforzándose para desarrollar la super-batería. <span>Cole Emmerich</span></h4>
+                <p>La que ofrece alta potencia, alta capacidad de almacenamiento, bajo costo, y que es sustentable.</p>
+              </div>
+            </div>
+
+            <div class="row schedule-item">
+              <div class="col-md-2"><time>02:00 PM</time></div>
+              <div class="col-md-10">
+                <div class="speaker">
+                  <img src="public/img/speakers/4.jpg" alt="Jack Christiansen">
+                </div>
+                <h4>Robótica. <span>Jack Christiansen</span></h4>
+                <p>Investigación, educación, y educación en investigación con robótica.</p>
+              </div>
+            </div>
+
+            <div class="row schedule-item">
+              <div class="col-md-2"><time>03:00 PM</time></div>
+              <div class="col-md-10">
+                <div class="speaker">
+                  <img src="public/img/speakers/5.jpg" alt="Alejandrin Littel">
+                </div>
+                <h4>Oportunidades y desafíos en la Educación Superior. <span>Alejandrin Littel</span></h4>
+                <p>Un enfoque Personal, Evolutivo y Experiencial.</p>
+              </div>
+            </div>
+
+            <div class="row schedule-item">
+              <div class="col-md-2"><time>04:00 PM</time></div>
+              <div class="col-md-10">
+                <div class="speaker">
+                  <img src="public/img/speakers/6.jpg" alt="Willow Trantow">
+                </div>
+                <h4>Desarrollo de tecnologías. <span>Willow Trantow</span></h4>
+                <p>Calzado ortopédico sostenible y asequible para países en desarrollo utilizando materiales múltiples funcionales sostenibles y tecnologías de Industria 4.0.</p>
+              </div>
+            </div>
+          </div>
+
+          <!-- Dia 2 -->
+          <div role="tabpanel" class="col-lg-9  tab-pane fade" id="day-2">
+
+          <div class="row schedule-item">
+              <div class="col-md-2"><time>09:30 AM</time></div>
+              <div class="col-md-10">
+                <h4>Bienvenida. </h4>
+                <p>Bienvenida de los Organizadores del Congreso.</p>
+              </div>
+            </div>
+
+            <div class="row schedule-item">
+              <div class="col-md-2"><time>10:00 AM</time></div>
+              <div class="col-md-10">
+                <div class="speaker">
+                  <img src="public/img/speakers/4.jpg" alt="Brenden Legros">
+                </div>
+                <h4>La Innovación. <span>Brenden Legros</span></h4>
+                <p>El Imperativo de Innovar para un desarrollo inclusivo y sustentable.</p>
+              </div>
+            </div>
+
+            <div class="row schedule-item">
+              <div class="col-md-2"><time>11:00 AM</time></div>
+              <div class="col-md-10">
+                <div class="speaker">
+                  <img src="public/img/speakers/5.jpg" alt="Hubert Hirthe">
+                </div>
+                <h4>Topografía de superficie. <span>Hubert Hirthe</span></h4>
+                <p>Uso de la Biología Básica para informar el Diseño de Dispositivos Médicos: Topografía de superficie.</p>
+              </div>
+            </div>
+
+            <div class="row schedule-item">
+              <div class="col-md-2"><time>12:00 AM</time></div>
+              <div class="col-md-10">
+                <div class="speaker">
+                  <img src="public/img/speakers/3.jpg" alt="Cole Emmerich">
+                </div>
+                <h4>Esforzándose para desarrollar la super-batería. <span>Cole Emmerich</span></h4>
+                <p>La que ofrece alta potencia, alta capacidad de almacenamiento, bajo costo, y que es sustentable.</p>
+              </div>
+            </div>
+
+            <div class="row schedule-item">
+              <div class="col-md-2"><time>02:00 PM</time></div>
+              <div class="col-md-10">
+                <div class="speaker">
+                  <img src="public/img/speakers/2.jpg" alt="Jack Christiansen">
+                </div>
+                <h4>Robótica. <span>Jack Christiansen</span></h4>
+                <p>Investigación, educación, y educación en investigación con robótica.</p>
+              </div>
+            </div>
+
+            <div class="row schedule-item">
+              <div class="col-md-2"><time>03:00 PM</time></div>
+              <div class="col-md-10">
+                <div class="speaker">
+                  <img src="public/img/speakers/1.jpg" alt="Alejandrin Littel">
+                </div>
+                <h4>Oportunidades y desafíos en la Educación Superior. <span>Alejandrin Littel</span></h4>
+                <p>Un enfoque Personal, Evolutivo y Experiencial.</p>
+              </div>
+            </div>
+
+            <div class="row schedule-item">
+              <div class="col-md-2"><time>04:00 PM</time></div>
+              <div class="col-md-10">
+                <div class="speaker">
+                  <img src="public/img/speakers/6.jpg" alt="Willow Trantow">
+                </div>
+                <h4>Desarrollo de tecnologías. <span>Willow Trantow</span></h4>
+                <p>Calzado ortopédico sostenible y asequible para países en desarrollo utilizando materiales múltiples funcionales sostenibles y tecnologías de Industria 4.0.</p>
+              </div>
+            </div>
+          </div>
+
+          <!-- Dia 3 -->
+          <div role="tabpanel" class="col-lg-9  tab-pane fade" id="day-3">
+
+          <div class="row schedule-item">
+              <div class="col-md-2"><time>09:30 AM</time></div>
+              <div class="col-md-10">
+                <h4>Bienvenida.</h4>
+                <p>Bienvenida de los Organizadores del Congreso.</p>
+              </div>
+            </div>
+
+            <div class="row schedule-item">
+              <div class="col-md-2"><time>10:00 AM</time></div>
+              <div class="col-md-10">
+                <div class="speaker">
+                  <img src="public/img/speakers/1.jpg" alt="Brenden Legros">
+                </div>
+                <h4>La Innovación. <span>Brenden Legros</span></h4>
+                <p>El Imperativo de Innovar para un desarrollo inclusivo y sustentable.</p>
+              </div>
+            </div>
+
+            <div class="row schedule-item">
+              <div class="col-md-2"><time>11:00 AM</time></div>
+              <div class="col-md-10">
+                <div class="speaker">
+                  <img src="public/img/speakers/2.jpg" alt="Hubert Hirthe">
+                </div>
+                <h4>Topografía de superficie. <span>Hubert Hirthe</span></h4>
+                <p>Uso de la Biología Básica para informar el Diseño de Dispositivos Médicos: Topografía de superficie.</p>
+              </div>
+            </div>
+
+            <div class="row schedule-item">
+              <div class="col-md-2"><time>12:00 AM</time></div>
+              <div class="col-md-10">
+                <div class="speaker">
+                  <img src="public/img/speakers/3.jpg" alt="Cole Emmerich">
+                </div>
+                <h4>Esforzándose para desarrollar la super-batería. <span>Cole Emmerich</span></h4>
+                <p>La que ofrece alta potencia, alta capacidad de almacenamiento, bajo costo, y que es sustentable.</p>
+              </div>
+            </div>
+
+            <div class="row schedule-item">
+              <div class="col-md-2"><time>02:00 PM</time></div>
+              <div class="col-md-10">
+                <div class="speaker">
+                  <img src="public/img/speakers/4.jpg" alt="Jack Christiansen">
+                </div>
+                <h4>Robótica. <span>Jack Christiansen</span></h4>
+                <p>Investigación, educación, y educación en investigación con robótica.</p>
+              </div>
+            </div>
+
+            <div class="row schedule-item">
+              <div class="col-md-2"><time>03:00 PM</time></div>
+              <div class="col-md-10">
+                <div class="speaker">
+                  <img src="public/img/speakers/5.jpg" alt="Alejandrin Littel">
+                </div>
+                <h4>Oportunidades y desafíos en la Educación Superior. <span>Alejandrin Littel</span></h4>
+                <p>Un enfoque Personal, Evolutivo y Experiencial.</p>
+              </div>
+            </div>
+
+            <div class="row schedule-item">
+              <div class="col-md-2"><time>04:00 PM</time></div>
+              <div class="col-md-10">
+                <div class="speaker">
+                  <img src="public/img/speakers/6.jpg" alt="Willow Trantow">
+                </div>
+                <h4>Desarrollo de tecnologías. <span>Willow Trantow</span></h4>
+                <p>Calzado ortopédico sostenible y asequible para países en desarrollo utilizando materiales múltiples funcionales sostenibles y tecnologías de Industria 4.0.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
+ 
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+
+
+ <!-- Footer -->
+
+ <?php
+require_once 'view/Header-Footer/Footer.php';
+?>
+  <a href="#" class="back-to-top"><i class="fa fa-angle-up"></i></a>
+
+  <!-- JavaScript Librarias -->
+  <script src="public/js/jquery/jquery.min.js"></script>
+  <script src="public/js/jquery/jquery-migrate.min.js"></script>
+  <script src="public/js/bootstrap/bootstrap.min.js"></script>
+  <script src="public/js/bootstrap/bootstrap.bundle.min.js"></script>
+  <script src="public/js/easing/easing.min.js"></script>
+  <script src="public/js/superfish/hoverIntent.js"></script>
+  <script src="public/js/superfish/superfish.min.js"></script>
+  <script src="public/js/wow/wow.min.js"></script>
+  <script src="public/js/venobox/venobox.min.js"></script>
+  <script src="public/js/owlcarousel/owl.carousel.min.js"></script>
+
+  <!-- Contactos -->
+  <script src="public/js/contactform.js"></script>
+
+  <!-- JS -->
+  <script src="public/js/main.js"></script>
 </body>
+
 </html>
