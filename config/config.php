16e780cf1fda56a93c10bba5b8a8666877692a78
Daniel250Gue <84732717+Daniel250Gue@users.noreply.github.com>
2022-11-06 14:59:39 -0500

    Conexion DB
    Conexion ON

---
+++ config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define("DB_HOST", "profsql.database.windows.net");
+define("DB", "Congreso_Db");
+define("DB_USER", "adminsql");
+define("DB_PASS", " AdmPfsql1");
+
+
+?>
--- model/db_c.php
+++ model/db_c.php
@@ -1,7 +1,3 @@
-
-
-
-
 <?php
 
 class Db
