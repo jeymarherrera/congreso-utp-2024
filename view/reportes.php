7f75cbd52cb54f866901a5bab19e6856eb48e081
jeymarherrera <jeymarherrera@gmail.com>
2022-11-20 13:16:03 -0500

    Update reportes.php

--- view/reportes.php
+++ view/reportes.php
@@ -40,21 +40,7 @@
 
     <!-- Chart Start -->
     <div class="container-fluid pt-4 px-4">
-        <div class="row g-4">
-            <div class="col-sm-12 col-xl-6">
-                <div class="bg-light rounded h-100 p-4">
-                    <h6 class="mb-4">Estadistica de Usuarios Internacionales Registrados</h6>
-                    <canvas id="worldwide-sales"></canvas>
-                </div>
-            </div>
-
-            <div class="col-sm-12 col-xl-6">
-                <div class="bg-light rounded h-100 p-4">
-                    <h6 class="mb-4">Estadisticas del Tipo de Usuario Registrados</h6>
-                    <canvas id="doughnut-chart"></canvas>
-                </div>
-            </div>
-        </div>
+        <h6 class="mb-4">* Los reportes son visualizados en Power BI</h6>
     </div>
 
     <?php
